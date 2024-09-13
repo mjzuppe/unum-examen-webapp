8c23a6c78bd27213846a35e694c81dc3e7eb8a8b
Mark Zuppe <zuppe@live.com>
2024-09-13 12:55:41 -0400

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind(),tailwind({
+    applyBaseStyles: false,
+  })],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "./src/styles/fonts.css";`,
+        },
+      },
+    },
+  },
+});
