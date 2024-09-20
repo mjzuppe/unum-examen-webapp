4d5e986f4a223ea171028aef33a40b148e124962
Mark Zuppe <zuppe@live.com>
2024-09-20 15:23:09 -0400

    update language config within astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://unumexamen.com',
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
   integrations: [react(), tailwind(),tailwind({
     applyBaseStyles: false,
   })],
