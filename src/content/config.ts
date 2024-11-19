af2444aa453f0e31c67702e2e81d6a8a28e0ee89
mjzuppe <zuppe@live.com>
2024-11-19 19:04:15 +0000

    integrate examin jsons into slug

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,4 +12,17 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const examination = defineCollection({
+	// Type-check frontmatter using a schema
+	// schema: z.object({
+	// 	title: z.string(),
+	// 	description: z.string(),
+	// 	// Transform string to Date object
+	// 	pubDate: z.coerce.date(),
+	// 	updatedDate: z.coerce.date().optional(),
+	// 	heroImage: z.string().optional(),
+	});
+
+// });
+
+export const collections = { blog, examination };
